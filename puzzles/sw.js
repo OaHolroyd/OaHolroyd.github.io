566ed81314ecff0f3b652158179d1c6a250b533e
oh <oaholroyd@gmail.com>
2023-01-22 11:39:34 +0000

    pwa fixes

--- puzzles/sw.js
+++ puzzles/sw.js
@@ -15,14 +15,18 @@ var filesToCache = [
   '/js/Random.js',
   '/js/Trie.js',
   '/js/word-wheel-view.js',
-  '/js/WordWheel.js'
+  '/js/WordWheel.js',
 ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
+      const stack = [];
+      assets.forEach(file => stack.push(
+          cache.add(file).catch(_=>console.error(`can't load ${file} to cache`))
+      ));
+      return Promise.all(stack);
     })
   );
 });
